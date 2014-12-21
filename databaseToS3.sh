64662be70d92ba74fb208136ae6f29bd031295bf
Nicholas Kreidberg <niczak@gmail.com>
2014-12-21 16:24:53 -0600

    Database backup script.
    Adding database backup script to repo, next up will be a script for
    backing up files, then adding features to each.

---
+++ databaseToS3.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# WordPress to S3 Bash Script
+# https://github.com/niczak/
+#
+# Configuration: Update the variables below with your credentials.
+#
+# Usage: Run directly from command line or as a cron job.
+#
+# sh ./wpS3Cron.sh
+#
+#
+
+mysqluser="MYSQL_USER"
+mysqlpass="MYSQL_PASS"
+mysqldb="MYSQL_DB"
+
+stamp=`date +"%m-%d-%y"`
+tempfile='/tmp/wp-backup.gz'
+
+s3bucket="s3://BUCKETNAME"
+s3file="$s3bucket/wp-backup-$stamp.gz"
+
+echo "\n... Dumping data"
+
+mysqldump -u "$mysqluser" -p"$mysqlpass" --force --opt --databases "$mysqldb" | gzip -c > "$tempfile"
+
+echo "\n... Uploading data"
+
+s3cmd put "$tempfile" "$s3file"
+
+echo "\n... Cleaning up"
+rm -f "$tempfile"
